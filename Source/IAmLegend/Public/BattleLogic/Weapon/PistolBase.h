5877ad231173e8830e2b05e21f17451355d48e1d
Cha0910 <ae20414@naver.com>
2026-05-13 10:50:47 +0900

    [BattleLogic 차재현] feat: 무기 타입 추가 - WeaponBase.h에 enum class의 EWeaponType 추가 - 무기의 타입을 반환하는 GetWeaponType 추가

--- Source/IAmLegend/Public/BattleLogic/Weapon/PistolBase.h
+++ Source/IAmLegend/Public/BattleLogic/Weapon/PistolBase.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -14,4 +14,6 @@ class IAMLEGEND_API APistolBase : public ARangedWeaponBase
 {
 	GENERATED_BODY()
 	
+public:
+	APistolBase();
 };
