8c331a5ff5a545ee2328745c01edd83c01c7bf6d
Cha0910 <ae20414@naver.com>
2026-05-14 21:38:29 +0900

    [BattleLogic 차재현] feat: 투사체 궤적 추가 - 투사체의 궤적을 그려주는 TrajectoryComponent 추가 - 궤적은 NS_Trajectory로 시각화 - ThrowableWeaponBase에 TrajectoryComponent를 추가 - HanPlayerChahacter의 StartAim(), StopAim()에서 장착 무기를 확인

--- Source/IAmLegend/Public/BattleLogic/Weapon/ThrowableWeaponBase.h
+++ Source/IAmLegend/Public/BattleLogic/Weapon/ThrowableWeaponBase.h
@@ -6,8 +6,6 @@
 #include "BattleLogic/WeaponBase.h"
 #include "ThrowableWeaponBase.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnWeaponThrown);
-
 /**
  * 
  */
@@ -17,17 +15,20 @@ class IAMLEGEND_API AThrowableWeaponBase : public AWeaponBase
 	GENERATED_BODY()
 
 protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class UTrajectoryComponent* TrajectoryComp;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Throwable")
 	TSubclassOf<class AWeaponProjectileBase> ProjectileClass; // 투척 시 생성할 투사체 클래스
 public:
-	UPROPERTY(BlueprintAssignable, Category = "Weapon|Event")
-	FOnWeaponThrown OnWeaponThrown;
-
 	AThrowableWeaponBase();
 	virtual void BeginPlay() override;
 	virtual void OnConstruction(const FTransform& Transform) override;
 
 	virtual void StartWeaponAttack() override;		// 기본 공격 시작
-	virtual void ThrowWeapon();						// 무기 투척 (ProjectileClass를 사용하여 투사체 생성)
 	virtual void WeaponInitFromData() override;
+
+	void ThrowWeapon();						// 무기 투척 (ProjectileClass를 사용하여 투사체 생성)
+
+	void EnableTrajectory(bool bEnable);	// 투사체 궤적 표시 활성화/비활성화
 };
