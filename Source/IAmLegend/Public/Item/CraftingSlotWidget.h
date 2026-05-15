ceae7163cec142c43ae6afc357f94e7d2058d067
blurqls <blurwuri@gmail.com>
2026-05-15 17:18:21 +0900

    [조합 및 인벤토리 수정] 쉘터에 조합대 배치, 조합대 초기 UI 설정

--- Source/IAmLegend/Public/Item/CraftingSlotWidget.h
+++ Source/IAmLegend/Public/Item/CraftingSlotWidget.h
@@ -21,22 +21,29 @@ public:
 protected:
 	virtual void NativeConstruct() override;
 
-	// 현재 슬롯이 담당하는 레시피 데이터
 	UPROPERTY()
 	TObjectPtr<UCraftRecipe> TargetRecipe;
+
+	// --- 재료 1 ---
+	UPROPERTY(meta = (BindWidget))
+	class UImage* Ingredient_1_Icon;
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* Ingredient_1_Count;
+
+	// --- 재료 2 ---
+	UPROPERTY(meta = (BindWidget))
+	class UImage* Ingredient_2_Icon;
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* Ingredient_2_Count;
+
+	// --- 결과물 및 버튼 ---
+	UPROPERTY(meta = (BindWidget))
+	class UImage* ResultIcon;
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* ResultNameText;
+	UPROPERTY(meta = (BindWidget))
+	class UButton* CraftButton;
 	
-    
-	// 결과물 아이콘
-	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<UImage> ResultIcon;
-
-	// 결과물 이름 또는 수량
-	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<UTextBlock> ResultNameText;
-
-	// 제작 버튼
-	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<UButton> CraftButton;
 
 	// 제작 버튼 클릭 시 실행될 함수
 	UFUNCTION()
