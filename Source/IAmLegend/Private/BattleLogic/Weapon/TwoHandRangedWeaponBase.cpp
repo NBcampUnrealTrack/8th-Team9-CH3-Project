9e3eff27bbc0f8840bc12b94080a35fd1ab5c0e2
KDam123 <127168329+KDam123@users.noreply.github.com>
2026-05-24 23:33:34 +0900

    no message

--- Source/IAmLegend/Private/BattleLogic/Weapon/TwoHandRangedWeaponBase.cpp
+++ Source/IAmLegend/Private/BattleLogic/Weapon/TwoHandRangedWeaponBase.cpp
@@ -58,52 +58,6 @@ void ATwoHandRangedWeaponBase::MeleeAttackTrace()
 	{
 		ProcessMeleeHits(HitResults);
 	}
-	/*
-	//if (!bIsMeleeAttacking) return;
-	if (GetWorld()->GetTimerManager().IsTimerActive(CoolDownTimerHandle)) return;
-
-	HitActors.Empty();
-
-	if (!OwnerCharacter || !OwnerCharacter->GetMesh()) return;
-
-	// 1. 기존 스켈레톤 소켓 위치 그대로 가져오기
-	FVector BaseStart = OwnerCharacter->GetMesh()->GetSocketLocation(FName("KickStart"));
-	FVector BaseEnd = OwnerCharacter->GetMesh()->GetSocketLocation(FName("KickEnd"));
-
-	// 2. [핵심 수정: 사거리 앞으로 늘리기 🚀]
-	// 플레이어가 바라보는 정면 방향 벡터를 구합니다.
-	FVector ForwardVector = OwnerCharacter->GetActorForwardVector();
-
-	// 소켓 위치를 기준으로 하되, 정면(Forward) 방향으로 궤적을 70cm씩 앞으로 더 밀어내고 연장합니다.
-	// 이렇게 하면 발 뒤쪽 판정은 사라지고, 발 앞 공간(좀비가 서 있는 곳) 깊숙이 판정이 파고듭니다.
-	FVector Start = BaseStart + (ForwardVector * 30.f);
-	FVector End = BaseEnd + (ForwardVector * 70.f);
-
-	TArray<FHitResult> HitResults;
-	FCollisionQueryParams Params;
-	Params.AddIgnoredActor(OwnerCharacter);
-	Params.AddIgnoredActor(this);
-
-	// 3. [핵심 수정: 두께 줄이기 🔪]
-	// 기존 25cm였던 두껍고 둥글둥글한 범위를 12cm로 줄여서 정타가 꽂히는 정교함을 줍니다.
-	float KickThickness = 12.0f;
-
-	// 트레이스 채널 실행
-	bool bHit = GetWorld()->SweepMultiByChannel(
-		HitResults,
-		Start,
-		End,
-		FQuat::Identity,
-		ATTACK_TRACE_CHANNEL,
-		FCollisionShape::MakeSphere(KickThickness),
-		Params
-	);
-
-	if (bHit)
-	{
-		ProcessMeleeHits(HitResults);
-	}
-	*/
 	/* 한기담 - 기존 방식 - 혹시 몰라 주석 처리 해두겠습니다.
 	if (!OwnerCharacter || !SkeletalMesh) return;
 
@@ -117,9 +71,6 @@ void ATwoHandRangedWeaponBase::MeleeAttackTrace()
 	// 시작점에서 데이터 에셋에 등록된 사거리(MeleeAttackRange)만큼 앞쪽을 끝점으로 설정합니다.
 	FVector End = Start + ForwardVector * MeleeAttackRange; 
 	
-	
-	
-
 	/* 메시 기준 벡터
 	// 메시 기준 피격 판정을 한다면 헤더의 메시 컴포넌트를 활성화하고 아래의 코드를 사용하면 됩니다.
 	FVector Start = Mesh->GetSocketLocation(FName("Root")); 
